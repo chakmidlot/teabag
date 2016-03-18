05ec962ecea36b31c67fbdb0776e1e5d519319eb
Mikhas Varantsou <meequz@gmail.com>
2016-03-18 14:18:26 +0300

    implement shredding with shred utility

--- teabag/shredder.py
+++ teabag/shredder.py
@@ -1,5 +1,17 @@
 import os
+import subprocess
 
 
 def remove_file(file_path):
-    os.remove(file_path)
+    command = ['shred', '-u', '-z', '-n', '7', file_path]
+    _execute(command)
+    assert not os.path.isfile(file_path)
+
+
+def _execute(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    code = process.wait()
+    if code != 0:
+        raise RuntimeError('Command failed')
+    output = [line.decode() for line in process.stdout]
+    return ''.join(output)
